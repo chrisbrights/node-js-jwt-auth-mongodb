2f3c86495b727ce09f5b376182f331c93ee85116
chrisbrights <softtalents1986@outlook.com>
2023-01-27 14:22:39 +0900

    using mailtrap instead of nodemailer

--- app/config/nodemailer.config.js
+++ app/config/nodemailer.config.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
 // const config = require("../config/auth.config");
 
@@ -13,25 +13,54 @@ const nodemailer = require("nodemailer");
 //     },
 // });
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'brook.cremin@ethereal.email',
-        pass: 'k6wQWeerbvXD1e3aPh'
-    }
-});
+// const transporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     auth: {
+//         user: 'brook.cremin@ethereal.email',
+//         pass: 'k6wQWeerbvXD1e3aPh'
+//     }
+// });
+
+// module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
+//     console.log("Check");
+//     transporter.sendMail({
+//         from: 'brook.cremin@ethereal.email',
+//         to: email,
+//         subject: "Please confirm your account",
+//         html: `<h1>Email Confirmation</h1>
+//         <h2>Hello ${name}</h2>
+//         <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+//         <a href='http://localhost:8081/confirm/${confirmationCode}'>Click Here</a>
+//         `
+//     }).catch(err => console.log(err));
+// };
+
+const { MailtrapClient } = require("mailtrap");
+
+// For this example to work, you need to set up a sending domain,
+// and obtain a token that is authorized to send from the domain
+const TOKEN = "c8e5ae89942792120d2262193b3f7c85";
+const SENDER_EMAIL = "softtalents1986@outlook.com";
+
+const client = new MailtrapClient({ token: TOKEN });
+const sender = { name: "Mailtrap Test", email: SENDER_EMAIL };
 
 module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
     console.log("Check");
-    transporter.sendMail({
-        from: 'brook.cremin@ethereal.email',
-        to: email,
-        subject: "Please confirm your account",
-        html: `<h1>Email Confirmation</h1>
-        <h2>Hello ${name}</h2>
-        <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
-        <a href='http://localhost:8081/confirm/${confirmationCode}'>Click Here</a>
-        `
-    }).catch(err => console.log(err));
+    client
+        .send({
+            from: sender,
+            to: [{ email: email }],
+            subject: "Please confirm your account",
+            html: `<h1>Email Confirmation</h1>
+                <h2>Hello ${name}</h2>
+                <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+                <a href='http://localhost:8081/confirm/${confirmationCode}'>Click Here</a>
+                `
+        })
+        .then(console.log, console.error);
 };
+
+
+
