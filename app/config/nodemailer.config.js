5ac7d5e811c45e6ab32325a7d9352fc3abb969bf
chrisbrights <softtalents1986@outlook.com>
2023-01-27 12:53:45 +0900

    using ethereal mail instead of wrong gmail.

--- app/config/nodemailer.config.js
+++ app/config/nodemailer.config.js
@@ -1,21 +1,31 @@
 const nodemailer = require("nodemailer");
-const config = require("../config/auth.config");
 
-const user = config.user;
-const pass = config.pass;
+// const config = require("../config/auth.config");
 
-const transport = nodemailer.createTransport({
-    service: "Gmail",
+// const user = config.user;
+// const pass = config.pass;
+
+// const transport = nodemailer.createTransport({
+//     service: "Gmail",
+//     auth: {
+//         user: user,
+//         pass: pass,
+//     },
+// });
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
     auth: {
-        user: user,
-        pass: pass,
-    },
+        user: 'brook.cremin@ethereal.email',
+        pass: 'k6wQWeerbvXD1e3aPh'
+    }
 });
 
 module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
     console.log("Check");
-    transport.sendMail({
-        from: user,
+    transporter.sendMail({
+        from: 'brook.cremin@ethereal.email',
         to: email,
         subject: "Please confirm your account",
         html: `<h1>Email Confirmation</h1>
